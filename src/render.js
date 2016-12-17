dcb0eb72f8dac534e787e7042a5791c8c809e23a
Justin Livi <justin@brinkbit.com>
2016-12-17 17:21:35 -0500

    Minor optimization

--- src/render.js
+++ src/render.js
@@ -23,66 +23,43 @@ module.exports = function createSketch( canvas ) {
     const width = canvas.width;
     const height = canvas.height;
     const ctx = canvas.getContext( '2d' );
-    const fps = 60;
-    const fpsInterval = 1000 / fps;
 
     let stopped = false;
-    let now;
-    let elapsed;
-    let then = window.performance.now();
 
     let seedcount = 50;
-    // overall distance from center
-    const maxh = height;
     let theta = 0;
-    const hm = []; // hue array
-    const sm = []; // saturation array
-    const lm = []; // lightness array
-    const dhm = []; // delta hue array
-    const dsm = []; // delta saturation array
-    const dlm = []; // delta lightness array
-    let dm = []; // distance array
-    const stm = []; // streakiness array
-
-    function changeDm( count ) {
-        dm[count] = constrain( dm[count] + random( -1, 1 ), 0, height );
-    }
-
-    function changeColor( count ) {
-        stm[count] += random( -0.01, 0.01 );
-        stm[count] = constrain( stm[count], 0.1, 1 );
-        dhm[count] += random( -stm[count], stm[count]);
-        dhm[count] = constrain( dhm[count], hm[count] - 20, hm[count] + 20 );
-        dsm[count] += random( -stm[count], stm[count]);
-        dsm[count] = constrain( dsm[count], sm[count] - 20, sm[count] + 20 );
-        dlm[count] += random( -stm[count], stm[count]);
-        dlm[count] = constrain( dlm[count], lm[count] - 20, lm[count] + 20 );
-    }
+    const hm = []; // hue
+    const sm = []; // saturation
+    const lm = []; // lightness
+    const dhm = []; // delta hue
+    const dsm = []; // delta saturation
+    const dlm = []; // delta lightness
+    let dm = []; // distance
+    const stm = []; // streakiness
 
     function blend( count, distance ) {
         let prev = seedcount - 1;
         if ( count < seedcount - 1 ) prev = count + 1;
         const formax = Math.abs( dm[prev] - distance );
-        changeColor( count );
+        const stmc = stm[count] = constrain( stm[count] + random( -0.01, 0.01 ), 0.1, 1 );
+        const dhmc = dhm[count] = constrain( dhm[count] + random( -stmc, stmc ), hm[count] - 20, hm[count] + 20 );
+        const dsmc = dsm[count] = constrain( dsm[count] + random( -stmc, stmc ), sm[count] - 20, sm[count] + 20 );
+        const dlmc = dlm[count] = constrain( dlm[count] + random( -stmc, stmc ), lm[count] - 20, lm[count] + 20 );
         for ( let count2 = 0; count2 < formax; count2++ ) {
             for ( let count3 = 0; count3 < 10 / 3 * stm[count] + 5 / 3; count3++ ) {
-                const hi = ( dhm[prev] - dhm[count]) / formax;
-                const si = ( dsm[prev] - dsm[count]) / formax;
-                const li = ( dlm[prev] - dlm[count]) / formax;
                 ctx.save();
                 ctx.translate( distance + count2 + random( -1, 1 ), random( -stm[count] * 10, stm[count] * 10 ));
-                ctx.rotate( random( Math.PI * 2 ));
                 ctx.beginPath();
                 ctx.fillStyle = `hsla(${
-                        Math.floor( dhm[count] + random( -1, 1 ) + ( count2 * hi ))
+                        Math.floor( dhmc + random( -1, 1 ) + count2 * ( dhm[prev] - dhmc ) / formax )
                     },${
-                        Math.floor( dsm[count] + random( -0.5, 0.5 ) + ( count2 * si ))
+                        Math.floor( dsmc + random( -0.5, 0.5 ) + count2 * ( dsm[prev] - dsmc ) / formax )
                     }%,${
-                        Math.floor( dlm[count] + random( -0.5, 0.5 ) + ( count2 * li ))
+                        Math.floor( dlmc + random( -0.5, 0.5 ) + count2 * ( dlm[prev] - dlmc ) / formax )
                     }%, ${
                         random( 2, 10 ) / 255
                     })`;
-                ctx.ellipse( 0, 0, random( 2, 10 + 5 * stm[count]), random( 2, 10 + 5 * stm[count]), 0, 0, 2 * Math.PI );
+                ctx.ellipse( 0, 0, random( 2, 10 + 5 * stmc ), random( 2, 10 + 5 * stmc ), 0, 0, random( Math.PI * 2 ));
                 ctx.fill();
                 ctx.restore();
             }
@@ -91,27 +68,24 @@ module.exports = function createSketch( canvas ) {
 
     function generate() {
         for ( let count = 0; count < seedcount; count++ ) {
-            changeDm( count );
+            dm[count] = constrain( dm[count] + random( -1, 1 ), 0, height );
         }
         dm[0] = 0;
         dm[seedcount - 1] = height;
         dm = sortBy( dm );
         for ( let count = 0; count < seedcount; count++ ) {
-            ctx.fillStyle = `hsl(${dhm[count]}, ${dsm[count]}, ${dsm[count]})`;
             blend( count, dm[count]);
         }
     }
 
     function draw() {
-        for ( let i = 0; i < 2; i++ ) {
-            if ( theta < width ) {
-                ctx.save();
-                ctx.translate( theta, 0 );
-                ctx.rotate( Math.PI / 2 );
-                generate();
-                ctx.restore();
-                theta++;
-            }
+        if ( theta < width ) {
+            ctx.save();
+            ctx.translate( theta, 0 );
+            ctx.rotate( Math.PI / 2 );
+            generate();
+            ctx.restore();
+            theta++;
         }
     }
 
@@ -127,10 +101,9 @@ module.exports = function createSketch( canvas ) {
             dhm[count] = hm[count];
             dsm[count] = sm[count];
             dlm[count] = lm[count];
-            dm[count] = random( maxh );
+            dm[count] = random( height );
             stm[count] = random( 0.1, 1 );
         }
-        // console.log( dm[1]);
         dm[0] = 0;
         dm[seedcount - 1] = height;
         dm = sortBy( dm );
@@ -140,16 +113,7 @@ module.exports = function createSketch( canvas ) {
     function animate() {
         if ( stopped ) return;
         raf( animate );
-        now = window.performance.now();
-        elapsed = now - then;
-
-        if ( elapsed > 2000 ) {
-            then = now - ( elapsed % fpsInterval );
-        }
-        else if ( elapsed > fpsInterval ) {
-            then = now - ( elapsed % fpsInterval );
-            draw( 1 + ( elapsed - fpsInterval ) / fpsInterval );
-        }
+        draw();
     }
 
     canvas.addEventListener( 'click', reset );
